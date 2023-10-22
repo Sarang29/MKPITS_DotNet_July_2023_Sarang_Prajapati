145c51a22942197bd1443167fcdd69c491230cca
sarang prajapati <sarangprajapati81@gmail.com>
2023-10-22 23:51:26 +0530

    Programs

---
+++ C#/Acpt_5_Num_Disp_Sum_of_5_num_ForLoop_Prac.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acpt_5_Num_Print_Sum_of_5_Num_ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int sum = 0;
+
+            for (int counter = 1; counter <= 5; counter++)
+            {
+                Console.Write("Enter Number : ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                sum = sum + counter;
+
+            }
+            Console.WriteLine("Sum = {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
