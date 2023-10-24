1092336a27929256943a301ffda3fd857fa12a36
sarang prajapati <sarangprajapati81@gmail.com>
2023-10-24 22:34:30 +0530

    Programs

---
+++ C#/Acpt_Length_Breadth_Disp_Area_Of_Traiangle_Prac.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acpt_Length_Breadth_Disp_Area_Of_Traiangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length, breadth, area;
+            Console.WriteLine("Enter Length");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Breadth");
+            breadth = Convert.ToInt32(Console.ReadLine());
+            area = length * breadth;
+            Console.WriteLine("Area Of Triangle = " + area);
+            Console.ReadKey();
+        }
+    }
+}
