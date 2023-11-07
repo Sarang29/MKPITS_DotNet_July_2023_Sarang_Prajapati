602ed35adb131e7fa6f6da839fc4860ca02f1586
sarang prajapati <sarangprajapati81@gmail.com>
2023-11-07 17:14:15 +0530

    Programs

---
+++ C#/Oops/Class_Calculate_Meth_Addition_Method_OverLoading.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Calculate_Meth_Addition_Method_OverLoading
+{
+    class Calculate
+    {
+        public void Addition(int num1,int num2)
+        {
+            int res = num1 + num2;
+            Console.WriteLine("Addition of Two Int Para = " + res);
+        }
+
+        public void Addition(float n1,float n2)
+        {
+            float res = n1 + n2;
+            Console.WriteLine("Addition of Two FLoat Para = " + res);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calculate = new Calculate();
+            Console.WriteLine("Additon of Two Int Para : ");
+            calculate.Addition(3, 4);
+
+            Console.WriteLine("Addition of two float :  ");
+
+                calculate.Addition(3.4f, 1.4f);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/Destructor_Example_Class_Employe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructor_Example_Class_Employe
+{
+    class Employe
+    {
+        public Employe()
+        {
+            Console.WriteLine("Constructor Called");
+        }
+        ~Employe()
+        {
+            Console.WriteLine("Destructor Called");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employe employe1 = new Employe();
+            Console.ReadKey();
+
+            Employe employe2 = new Employe();
+            Console.ReadKey();
+        }
+    }
+}
