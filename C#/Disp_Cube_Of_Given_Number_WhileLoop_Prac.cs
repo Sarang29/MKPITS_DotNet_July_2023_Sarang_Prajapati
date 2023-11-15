75aa04a4080ffeb1838ba9f8ed2af0a68467470c
sarang prajapati <sarangprajapati81@gmail.com>
2023-11-15 23:40:04 +0530

    Programs

---
+++ C#/Disp_Cube_Of_Given_Number_WhileLoop_Prac.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disp_Cube_Of_Given_Number_WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int counter=1;
+            int cube = 0;
+
+            Console.WriteLine("Enter Number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while(counter <= num)
+            {
+                cube = counter * counter * counter;
+                Console.WriteLine("cube = {0}", cube);
+                counter++;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
