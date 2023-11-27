27ab2044a84d7c6b2a2cf050b2e4cff07ec45d1d
sarang prajapati <sarangprajapati81@gmail.com>
2023-11-27 22:53:55 +0530

    Programs

---
+++ C#/Oops/Generic_List_Language_Insert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_List_Language_Insert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> langs = new List<string>();
+
+            langs.Add("MongoDB");
+            langs.Add("Java");
+            langs.Add("Dotnet");
+            langs.Add("PHP");
+            langs.Add("Csharp");
+
+            langs.Insert(3, "SQL");
+
+            foreach (string s in langs)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Oops/Generic_List_Languages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_List_Languages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> langs = new List<string>();
+            langs.Add("Csharp");
+            langs.Add("Java");
+            langs.Add("PHP");
+            langs.Add("Python");
+
+            Console.WriteLine(langs.Contains("Ruby"));
+
+            Console.WriteLine(langs[0]);
+            Console.WriteLine(langs[1]);
+
+            foreach(string s in langs)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
