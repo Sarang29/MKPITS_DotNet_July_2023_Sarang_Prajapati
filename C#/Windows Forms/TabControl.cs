df3cb3ebfeb14d53a98309be927aec6181d986df
sarang prajapati <sarangprajapati81@gmail.com>
2023-12-07 18:13:29 +0530

    Programs

---
+++ C#/Windows Forms/TabControl.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            maskedTextBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name : " + textBox1.Text + "\n");
+            sb.Append("Mobile No : " + maskedTextBox1.Text + "\n");
+            sb.Append("Email : " + textBox2.Text + "\n");
+
+            label11.Text=sb.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb1 = new StringBuilder();
+            sb1.Append("Student Name : " + textBox3.Text + "\n");
+            sb1.Append("Mobile No : " + maskedTextBox2.Text + "\n");
+            sb1.Append("Class You Want To Take Admission : " + textBox4.Text + "\n");
+            sb1.Append("Parents Name : " + textBox5.Text + "\n");
+            sb1.Append("Parents Mobile No : " + maskedTextBox3.Text + "\n");
+            sb1.Append("Email : " + textBox6.Text + "\n");
+
+            label10.Text = sb1.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox7.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+    }
+}
---
+++ C#/Windows Forms/Text_Editor.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fn=openFileDialog1.FileName;
+                MessageBox.Show(fn);
+                if(fn.EndsWith("rtf")|| fn.EndsWith("jpg"))
+                {
+                    richTextBox1.LoadFile(fn);
+                }
+                else
+                {
+                    MessageBox.Show("Cannot Open This File");
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                string fn = saveFileDialog1.FileName;
+                MessageBox.Show(fn);
+                if(fn.EndsWith("rtf"))
+                {
+                    richTextBox1.SaveFile(saveFileDialog1.FileName);
+                    MessageBox.Show("File Saved Succesfully");
+                }
+                else
+                {
+                    MessageBox.Show("Cannot Save File of This Format");
+                }
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog()== DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(fontDialog1.ShowDialog()==DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+            }
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome To Text Editor");
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+       
+        }
+    }
+}
