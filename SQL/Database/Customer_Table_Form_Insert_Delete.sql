f3c859198d903600036020012c5b1acb5f24325d
sarang prajapati <sarangprajapati81@gmail.com>
2024-01-06 22:09:04 +0530

    Tables

---
+++ SQL/Database/Customer_Table_Form_Insert_Delete.sql
@@ -0,0 +1,8 @@
+create table customer(
+CustId int primary key,
+CustName varchar(max),
+CustAddress varchar(max),
+MobileNo varchar(max),
+EmailId varchar(max))
+
+select * from customer
---
+++ SQL/Database/Product_Table_Disconnect_Enviro.sql
@@ -0,0 +1,8 @@
+create table products(
+ProdId int primary key,
+ProdName varchar(max),
+Price int,
+Quantity int)
+
+select * from products
+
